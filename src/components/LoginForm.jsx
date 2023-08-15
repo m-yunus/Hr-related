506a4a022567e2a993b4d2beefcba54de36ef4a2
Yunus <sanuyoonus@gmail.com>
2023-08-15 15:22:58 +0530

    authenticaton of registraion and login

--- src/components/LoginForm.jsx
+++ src/components/LoginForm.jsx
@@ -1,25 +1,69 @@
 import "../assets/css/login.css";
-import { Link } from "react-router-dom";
+import { Link,  useNavigate } from "react-router-dom";
 import { FaUser, FaEye } from "react-icons/fa";
+import { useState } from "react";
+import axios from "axios";
+import { BaseUrl } from "../ApiService/ApiService";
 const LoginForm = () => {
+  const [loginData, setLoginData] = useState({
+    username: '',
+    password: '',
+  });
+  
+  const Loginurl=`${BaseUrl}/api/user/login`
+
+  const navigate=useNavigate();
+  const handleonchange = (e) => {
+    const { name, value } = e.target;
+    setLoginData((prevData) => ({
+      ...prevData,
+      [name]: value,
+    }));
+  };
+  const handlelogin = (e) => {
+    e.preventDefault();
+
+    const urlEncodedData = new URLSearchParams();
+    urlEncodedData.append('username', loginData.username);
+    urlEncodedData.append('password', loginData.password);
+
+    axios
+      .post(Loginurl, urlEncodedData)
+      .then((response) => {
+        if (response.status === 200) {
+          const token = response.data.token; 
+          sessionStorage.setItem('token', token);
+        
+          navigate('/personalize');
+        }
+      })
+      .catch((err) => {
+        console.log(err);
+        
+      });
+  };
+
   return (
     <>
       <div className="login-section">
         <div className="login-form">
           <h3 style={{ textAlign: "center", padding: "20px" }}>Login</h3>
+          <form onSubmit={handlelogin}>
+
+          
           <div className="input-container">
-            <input type="text" placeholder="Username" />
+            <input type="text" placeholder="Username" name="username" onChange={handleonchange} value={loginData.username} />
             <span className="icon">
               <FaUser />
             </span>
           </div>
           <div className="input-container">
-            <input type="password" placeholder="Password" />
+            <input type="password" placeholder="Password" name="password" onChange={handleonchange}  value={loginData.password} />
             <span className="icon">
               <FaEye />
             </span>
           </div>
-         <Link to="/personalize"><button>Login</button> </Link> 
+         <button type="submit">Login</button>
           <div className="forgot-register-section">
             <p>
               <a href="">Forgot Password?</a>
@@ -30,6 +74,7 @@ const LoginForm = () => {
               </Link>
             </p>
           </div>
+          </form>
         </div>
       </div>
     </>
--- src/components/RegisterForm.jsx
+++ src/components/RegisterForm.jsx
@@ -1,45 +1,100 @@
-import { FaEye, FaUser } from "react-icons/fa"
-import {MdEmail} from "react-icons/md"
-import { Link } from "react-router-dom"
-
+import axios from "axios";
+import { useState } from "react";
+import { FaEye, FaUser } from "react-icons/fa";
+import { MdEmail } from "react-icons/md";
+import { Link } from "react-router-dom";
+import { BaseUrl } from "../ApiService/ApiService";
 
 const RegisterForm = () => {
-  return (
-   <>
-    <div className="login-section">
-        <div className="login-form">
-          <h3 style={{ textAlign: "center", padding: "20px" }}>Register</h3>
-          <div className="input-container">
-            <input type="text" placeholder="name" />
-            <span className="icon">
-              <FaUser />
-            </span>
-          </div>
-          <div className="input-container">
-            <input type="email" placeholder="E-mail" />
-            <span className="icon">
-              <MdEmail />
-            </span>
-          </div>
-          <div className="input-container">
-            <input type="password" placeholder="Password" />
-            <span className="icon">
-              <FaEye />
-            </span>
-          </div>
-          <button>Register</button>
-          <div className="forgot-register-section">
-            
-            <p>
-              <Link to="/">
-                <p>Already have an account ? </p>
-              </Link>
-            </p>
-          </div>
-        </div>
-      </div>
-   </>
-  )
-}
+  const [register, setRegister] = useState({
+    username: "",
+    password: "",
+    email: "",
+    fullName: "",
+  });
 
-export default RegisterForm
+  const RegisterUrl =`${BaseUrl}/api/user/register`;
+
+
+  const handleInput = (event) => {
+    event.preventDefault();
+    setRegister({ ...register, [event.target.name]: event.target.value });
+  };
+  
+  const handleSubmit = async (event) => {
+    event.preventDefault();
+    
+    try {
+      const response = await axios.post(RegisterUrl, register);
+      console.log("Registration successful:", response.data);
+    } catch (error) {
+      console.error("Registration failed:", error);
+    }
+  };
+
+  return (
+    <>
+      <div className="login-section">
+        <form onSubmit={handleSubmit}>
+          <div className="login-form">
+            <h3 style={{ textAlign: "center", padding: "20px" }}>Register</h3>
+            <div className="input-container">
+              <input
+                type="text"
+                placeholder="username"
+                onChange={handleInput}
+                name="username"
+              />
+              <span className="icon">
+                <FaUser />
+              </span>
+            </div>
+            <div className="input-container">
+              <input
+                type="text"
+                placeholder="Fullname"
+                onChange={handleInput}
+                name="fullName"
+              />
+              <span className="icon">
+                <FaUser />
+              </span>
+            </div>
+            <div className="input-container">
+              <input
+                type="email"
+                placeholder="E-mail"
+                onChange={handleInput}
+                name="email"
+              />
+              <span className="icon">
+                <MdEmail />
+              </span>
+            </div>
+            <div className="input-container">
+              <input
+                type="password"
+                placeholder="Password"
+                onChange={handleInput}
+                name="password"
+              />
+              <span className="icon">
+                <FaEye />
+              </span>
+            </div>
+            <button type="submit">Register</button>
+            <div className="forgot-register-section">
+              <p>
+                <Link to="/">
+                  <p>Already have an account ? </p>
+                </Link>
+              </p>
+            </div>
+          </div>
+        </form>
+      </div>
+    </>
+  );
+};
+
+export default RegisterForm;
