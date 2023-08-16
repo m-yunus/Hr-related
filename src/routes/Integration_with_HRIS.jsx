97bcbd46e081078f837e0c0ba5944d3ed3904ed4
Yunus <sanuyoonus@gmail.com>
2023-08-16 15:20:42 +0530

    integration with Hris submitted form

--- src/routes/Integration_with_HRIS.jsx
+++ src/routes/Integration_with_HRIS.jsx
@@ -1,105 +1,142 @@
+import axios from "axios";
 import { useState } from "react";
+import { BaseUrl } from "../ApiService/ApiService";
 
 const Integration_with_HRIS = () => {
-  const [selectedFile, setSelectedFile] = useState(null);
+  const [selectedFile, setSelectedFile] = useState({});
+  const [integrationWithHr, setIntegrationwithHr] = useState(false);
 
-  const handleFileChange = (event) => {
-    setSelectedFile(event.target.files[0]);
+  const handleFileChange = (e) => {
+    const file = e.target.files[0];
+    setSelectedFile(file);
+    console.log(file);
+  };
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    const headers = {
+      "x-access-token": sessionStorage.getItem("token"),
+     
+    };
+    const formData = new FormData();
+    formData.append("integration_with_hr", integrationWithHr);
+    formData.append("integration_image", selectedFile);
+console.log(formData);
+console.log("in",integrationWithHr);
+console.log("out",selectedFile);
+    try {
+      const res = await axios.post(
+        `${BaseUrl}/api/personalize/integration-with-hr`,
+        formData,
+        { headers }
+      );
+      console.log("Integration data submitted successfully:", res);
+    } catch (error) {
+      console.error("Error submitting form data:", error);
+    }
   };
   return (
     <>
-     <div className="wrapper-right">
-      <form action="">
-      <div className="dash-right-top">
-        <div className="pathname">
-          <h3>{location.pathname}</h3>
-        </div>
-        <input type="submit" className="update-button" value="Update" />
-      </div>
-      <div className="content-container">
-        <div className="heading-container">
-          <h4>Integration With HRIS</h4>
-          <div className="underline-grey"></div>
-        </div>
-        <table className="salary-table">
-          <tbody>
-            <tr>
-              <td>
-                <h5>Do You Want Salary Components?</h5>
-              </td>
-              <td>
-                <div className="radio-button-group">
-                  <input
-                    type="radio"
-                    name="salaryComponents"
-                    value="yes"
-                    defaultChecked
-                  />{" "}
-                  Yes
-                  <input type="radio" name="salaryComponents" value="no" /> No
-                </div>
-              </td>
-            </tr>
-            <tr>
-              <td>
-                <h5>Get Data From</h5>
-              </td>
-              <td>
-                <input type="text" placeholder="location" />
-              </td>
-            </tr>
-            <tr>
-              <td>
-                <h5>Store letters</h5>
-              </td>
-              <td>
-                <input type="text" placeholder="location" />
-              </td>
-            </tr>
-            <tr>
-              <td>
-                <h5>store Payroll extract </h5>
-              </td>
-              <td>
-                <input type="text" placeholder="location" />
-              </td>
-            </tr>
-          </tbody>
-        </table>
-        <div className="heading-container">
-          <h4>User Photos</h4>
-          <div className="underline-grey"></div>
-        </div>
-        <table className="salary-table">
-          <tbody>
-            <tr>
-              <td>
-                <h5>User Photos </h5>
-              </td>
-              <td>
-                <div className="file-input-container">
-                  <input
-                    type="file"
-                    id="file-input"
-                    className="file-input"
-                    onChange={handleFileChange}
-                  />
-                  <label htmlFor="file-input" className="file-input-label">
-                    Choose File
-                  </label>
-                </div>
+      <div className="wrapper-right">
+        <form onSubmit={handleSubmit}>
+          <div className="dash-right-top">
+            <div className="pathname">
+              <h3>{location.pathname}</h3>
+            </div>
+            <input type="submit" className="update-button" value="Update" />
+          </div>
+          <div className="content-container">
+            <div className="heading-container">
+              <h4>Integration With HRIS</h4>
+              <div className="underline-grey"></div>
+            </div>
+            <table className="salary-table">
+              <tbody>
+                <tr>
+                  <td>
+                    <h5>Do You Want Integration with HRIS?</h5>
+                  </td>
+                  <td>
+                    <div className="radio-button-group">
+                      <input
+                        type="radio"
+                        name="IntegrationHRIS"
+                        value="true"
+                        checked={integrationWithHr}
+                        onChange={() => setIntegrationwithHr(true)}
+                      />{" "}
+                      Yes
+                      <input
+                        type="radio"
+                        name="IntegrationHRIS"
+                        value="false"
+                        checked={!integrationWithHr}
+                        onChange={() => setIntegrationwithHr(false)}
+                      />{" "}
+                      No
+                    </div>
+                  </td>
+                </tr>
+                <tr>
+                  <td>
+                    <h5>Get Data From</h5>
+                  </td>
+                  <td>
+                    <input type="text" placeholder="location" />
+                  </td>
+                </tr>
+                <tr>
+                  <td>
+                    <h5>Store letters</h5>
+                  </td>
+                  <td>
+                    <input type="text" placeholder="location" />
+                  </td>
+                </tr>
+                <tr>
+                  <td>
+                    <h5>store Payroll extract </h5>
+                  </td>
+                  <td>
+                    <input type="text" placeholder="location" />
+                  </td>
+                </tr>
+              </tbody>
+            </table>
+            <div className="heading-container">
+              <h4>User Photos</h4>
+              <div className="underline-grey"></div>
+            </div>
+            <table className="salary-table">
+              <tbody>
+                <tr>
+                  <td>
+                    <h5>User Photos </h5>
+                  </td>
+                  <td>
+                    <div className="file-input-container">
+                      <input
+                        type="file"
+                        id="file-input"
+                        className="file-input"
+                        onChange={handleFileChange}
+                      />
+                      <label htmlFor="file-input" className="file-input-label">
+                        Choose File
+                      </label>
+                    </div>
 
-                {selectedFile && (
-                  <div>
-                    <p>Selected file: {selectedFile.name}</p>
-                  </div>
-                )}
-              </td>
-            </tr>
-          </tbody>
-        </table>
-      </div>
-      </form>
+                    {selectedFile && (
+                      <div>
+                        <p>Selected file: {selectedFile.name}</p>
+                      </div>
+                    )}
+                  </td>
+                </tr>
+              </tbody>
+            </table>
+          </div>
+        </form>
       </div>
     </>
   );
--- src/routes/Salary_components.jsx
+++ src/routes/Salary_components.jsx
@@ -1,8 +1,35 @@
+import axios from "axios";
+import {  useState } from "react";
+import { BaseUrl } from "../ApiService/ApiService";
+
 const Salary_components = () => {
+
+  const [salaryComponent, setSalaryComponent] = useState(false);
+  const [meritAppliedOn, setMeritAppliedOn] = useState({
+    value: "All_Components",
+    component: "",
+  });
+ 
+const handleSubmit=async(e)=>{
+e.preventDefault();
+const headers = {
+  "x-access-token": sessionStorage.getItem("token"),
+};
+const salryformData={
+  salary_component: salaryComponent,
+  merit_applied_on: meritAppliedOn,
+}
+try {
+  await axios.post(`${BaseUrl}/api/personalize/salary-component`,salryformData,{headers})
+  console.log('Form data submitted successfully:',salryformData);
+} catch (error) {
+  console.error('Error submitting form data:', error);
+}
+}
   return (
     <>
       <div className="wrapper-right">
-        <form action="">
+        <form onSubmit={handleSubmit}>
           <div className="dash-right-top">
             <div className="pathname">
               <h3>{location.pathname}</h3>
@@ -22,14 +49,22 @@ const Salary_components = () => {
                     <span>Do You Want Salary Components?</span>
                   </label>
                   <div className="radio-button-group">
-                    <input
-                      type="radio"
-                      name="salaryComponents"
-                      value="yes"
-                      defaultChecked
-                    />{" "}
-                    Yes
-                    <input type="radio" name="salaryComponents" value="no" /> No
+                  <input
+                    type="radio"
+                    name="salaryComponents"
+                    value="true"
+                    checked={salaryComponent}
+                    onChange={() => setSalaryComponent(true)}
+                  />{" "}
+                  Yes
+                  <input
+                    type="radio"
+                    name="salaryComponents"
+                    value="false"
+                    checked={!salaryComponent}
+                    onChange={() => setSalaryComponent(false)}
+                  />{" "}
+                  No
                   </div>
                 </div>
               </li>
@@ -43,20 +78,41 @@ const Salary_components = () => {
                     style={{ paddingLeft: "43px" }}
                   >
                     <input
-                      type="radio"
-                      name="merit"
-                      value="yes"
-                      defaultChecked
-                    />{" "}
-                    Yes
-                    <input type="radio" name="merit" value="no" /> No
+                    type="radio"
+                    name="merit"
+                    value="All component"
+                    checked={meritAppliedOn.value === "All_Components"}
+                    onChange={() =>
+                      setMeritAppliedOn({ ...meritAppliedOn, value: "All_Components" })
+                    }
+                  />{" "}
+                  All Components
+                  <input
+                    type="radio"
+                    name="merit"
+                    value="selectedcomponent"
+                    checked={meritAppliedOn.value === "selectedComponents"}
+                    onChange={() =>
+                      setMeritAppliedOn({ ...meritAppliedOn, value: "selectedComponents" })
+                    }
+                  />{" "}
+                  selectedcomponent
                   </div>
                 </div>
               </li>
 
               <li className="component-box">
               <label htmlFor="componentname">Component name</label>
-                <input type="text" />
+              <input
+                type="text"
+                value={meritAppliedOn.component}
+                onChange={(e) =>
+                  setMeritAppliedOn({
+                    ...meritAppliedOn,
+                    component: e.target.value,
+                  })
+                }
+              />
               </li>
             </ul>
           </div>
