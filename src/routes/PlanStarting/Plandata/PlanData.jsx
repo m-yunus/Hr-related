d7da3f1ab87ed50f789e61aaef4f02040d81cd64
Yunus <sanuyoonus@gmail.com>
2023-08-19 12:24:00 +0530

    console corections

--- src/routes/PlanStarting/Plandata/PlanData.jsx
+++ src/routes/PlanStarting/Plandata/PlanData.jsx
@@ -1,14 +1,14 @@
 import  { useState } from 'react';
-import Navbar from '../../../../layout/Navbar';
-import BonusIncentives from '../../BonusIncentives/BonusIncentives';
-import Component_Currency from '../../Component&Currency/Component_Currency';
-import Lumpsumcorrection from '../../Lumpsum&correction/Lumpsumcorrection';
-import ParityGuideline from '../../ParityandGuideline/ParityGuideline';
-import Promotion from '../../Promotion/Promotion';
-import Proration from '../../Prorations/Proration';
-import SplitandMatrix from '../../SplitandMatrix/splitandMatrix';
-import CycleEligibility from '../../Userplan/PlanData/cycleEligibility/CycleEligibility';
-import PayGroups from '../../paygroups/PayGroups';
+import Navbar from '../../../layout/Navbar';
+import BonusIncentives from '../BonusIncentives/BonusIncentives';
+import Component_Currency from '../Component&Currency/Component_Currency';
+import Lumpsumcorrection from '../Lumpsum&correction/Lumpsumcorrection';
+import ParityGuideline from '../ParityandGuideline/ParityGuideline';
+import Promotion from '../Promotion/Promotion';
+import Proration from '../Prorations/Proration';
+import SplitandMatrix from '../SplitandMatrix/splitandMatrix';
+import CycleEligibility from '../Userplan/PlanData/cycleEligibility/CycleEligibility';
+import PayGroups from '../paygroups/PayGroups';
 
 const Plandata = () => {
   const [currentStep, setCurrentStep] = useState(0);
@@ -59,7 +59,7 @@ const[PlanData,setPlandataValues]=useState({
     setCurrentStep(currentStep + 1);
     console.log(currentStep);
   };
-
+console.log(PlanData,setPlandataValues());
   const handleBack = () => {
     setCurrentStep(currentStep - 1);
 
