08e5076557ec061173aa47b6bc2df3a6aa19dfe3
Yunus <sanuyoonus@gmail.com>
2023-08-21 11:54:26 +0530

    split and matrix

--- src/routes/PlanStarting/Plandata/PlanData.jsx
+++ src/routes/PlanStarting/Plandata/PlanData.jsx
@@ -6,7 +6,7 @@ import Lumpsumcorrection from '../Lumpsum&correction/Lumpsumcorrection';
 import ParityGuideline from '../ParityandGuideline/ParityGuideline';
 import Promotion from '../Promotion/Promotion';
 import Proration from '../Prorations/Proration';
-import SplitandMatrix from '../SplitandMatrix/splitandMatrix';
+import SplitandMatrix from '../SplitandMatrix/SplitandMatrix';
 import CycleEligibility from '../Userplan/PlanData/cycleEligibility/CycleEligibility';
 import PayGroups from '../paygroups/PayGroups';
 import Time_based_bonus from '../Time_based_Bonus/Time_based_bonus';
